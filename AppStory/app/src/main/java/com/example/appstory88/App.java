f7eb41e4bf9991bfa226119d5d7ef7ad7006c8d5
tuannghia16 <tuannghia1603@gmail.com>
2023-06-22 01:21:48 +0800

    commit add app

--- AppStory/app/src/main/java/com/example/appstory88/App.java
+++ AppStory/app/src/main/java/com/example/appstory88/App.java
@@ -10,7 +10,11 @@ import com.example.appstory88.utils.MyDebugTree;
 import timber.log.Timber;
 
 public class App extends Application {
-
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        initLog();
+    }
 
     private void initLog() {
         if (BuildConfig.DEBUG) {
